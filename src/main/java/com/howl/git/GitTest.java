f2fe3f551dea4c34d191dfad7bd68b1f52aff40d
howl1992 <64535954+howl1992@users.noreply.github.com>
2021-07-01 01:59:48 +0800

    pull commit

--- src/main/java/com/howl/git/GitTest.java
+++ src/main/java/com/howl/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3-hotfix");
         System.out.println("hello master-test");
         System.out.println("hello git3-hotfix-test");
+        System.out.println("pull test");
     }
 }
